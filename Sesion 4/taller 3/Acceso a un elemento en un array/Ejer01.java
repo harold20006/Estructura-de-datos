72af4afdb91643c1fba2e5b026447364cc5f88e8
Harold Roldan Vargas <167786228+harold20006@users.noreply.github.com>
2025-02-27 16:12:18 +0000

    taller

---
+++ Sesion 4/taller 3/Acceso a un elemento en un array/Ejer01.java
@@ -0,0 +1,52 @@
+import java.util.Scanner;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class Ejer01{
+
+    public static void encabezadopgm() {
+        // Datos del encabezado
+        String nombre = "Harold Roldan Vargas";
+        String campus = "Campus Cali, U. Cooperativa de Colombia";
+        String repositorioGit = "https://congenial-capybara-4jq59xwjgjg6cg5j.github.dev/";
+
+        // Obtener la fecha y hora actual
+        LocalDateTime ahora = LocalDateTime.now();
+        DateTimeFormatter formateador = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm:ss");
+        String fechaHora = ahora.format(formateador);
+
+        // imprimir encabezado
+        System.out.println();
+        System.out.println("+----------------------------------------");
+        System.out.println("| 👤 Nombre: " + nombre);
+        System.out.println("| 🎓 Campus: " + campus);
+        System.out.println("| 📅 Fecha y hora: " + fechaHora);
+        System.out.println("| 📂 Repositorio Git: " + repositorioGit);
+        System.out.println("+----------------------------------------");
+        System.out.println();
+    }
+
+    public static int obtenerPrimerElemento(int[] arr) {
+        return arr[0];
+    }
+
+    public static void main(String[] args) {
+        encabezadopgm();
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Ingrese el tamaño de la lista de pacientes: ");
+        int n = scanner.nextInt();
+        int[] arr = new int[n];
+
+        System.out.println("Ingrese el nombre los pacientes:");
+        for (int i = 0; i < n; i++) {
+            arr[i] = scanner.nextInt();
+        }
+
+        System.out.println("El primer elemento del array es: " + obtenerPrimerElemento(arr));
+
+        scanner.close();
+    }
+
+
+}
