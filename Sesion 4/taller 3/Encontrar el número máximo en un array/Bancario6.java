10f1cf6e4d431be3ee3970b13601772984d2706f
Harold Roldan Vargas <167786228+harold20006@users.noreply.github.com>
2025-03-02 06:55:21 +0000

    taller

--- Sesion 4/taller 3/Encontrar el número máximo en un array/Bancario6.java
+++ Sesion 4/taller 3/Encontrar el número máximo en un array/Bancario6.java
@@ -1,20 +1,31 @@
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 public class Bancario6 {
-    public static void encabezadopgm() {
+    
+     public static void encabezadopgm() {
         // Datos del encabezado
         String nombre = "Harold Roldan Vargas";
         String campus = "Campus Cali, U. Cooperativa de Colombia";
         String repositorioGit = "https://congenial-capybara-4jq59xwjgjg6cg5j.github.dev/";
+
+        // Obtener la fecha y hora actual
+        LocalDateTime ahora = LocalDateTime.now();
+        DateTimeFormatter formateador = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm:ss");
+        String fechaHora = ahora.format(formateador);
 
         // imprimir encabezado
         System.out.println();
         System.out.println("+----------------------------------------");
         System.out.println("| 👤 Nombre: " + nombre);
         System.out.println("| 🎓 Campus: " + campus);
+        System.out.println("| 📅 Fecha y hora: " + fechaHora);
         System.out.println("| 📂 Repositorio Git: " + repositorioGit);
         System.out.println("+----------------------------------------");
         System.out.println();
     }
+
+  
 
     public static int maximo(int[] numeros) {
         int max = numeros[0];
