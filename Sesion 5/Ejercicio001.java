dde1a4a55ada2d2252351e07ed6487566f21de1d
Harold Roldan Vargas <167786228+harold20006@users.noreply.github.com>
2025-03-03 13:27:32 +0000

    taller

--- Sesion 5/Ejercicio001.java
+++ Sesion 5/Ejercicio001.java
@@ -1,4 +1,29 @@
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 public class Ejercicio001 {
+     public static void encabezadopgm() {
+        // Datos del encabezado
+        String nombre = "Harold Roldan Vargas";
+        String campus = "Campus Cali, U. Cooperativa de Colombia";
+        String repositorioGit = "https://congenial-capybara-4jq59xwjgjg6cg5j.github.dev/";
+
+        // Obtener la fecha y hora actual
+        LocalDateTime ahora = LocalDateTime.now();
+        DateTimeFormatter formateador = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm:ss");
+        String fechaHora = ahora.format(formateador);
+
+        // imprimir encabezado
+        System.out.println();
+        System.out.println("+----------------------------------------");
+        System.out.println("| 👤 Nombre: " + nombre);
+        System.out.println("| 🎓 Campus: " + campus);
+        System.out.println("| 📅 Fecha y hora: " + fechaHora);
+        System.out.println("| 📂 Repositorio Git: " + repositorioGit);
+        System.out.println("+----------------------------------------");
+        System.out.println();
+    }
+
 
     public static class Transaccion {
 
@@ -116,6 +141,8 @@ public class Ejercicio001 {
     public class PruebaListaEnlazada {
 
         public static void main(String[] args) {
+
+            encabezadopgm();
     
             ListaEnlazadaTransacciones lista = new ListaEnlazadaTransacciones();
     
