a935bc6f4bc09c96afaa7169cd94fe7503d82b8c
Harold Roldan Vargas <167786228+harold20006@users.noreply.github.com>
2025-05-04 21:51:28 +0000

    ruby

---
+++ Sesion11/ejer2/Encabezado.rb
@@ -0,0 +1,21 @@
+require 'time'
+
+def imprimir_encabezado
+   # Datos del encabezado
+   nombre = "Harold Roldan Vargas"
+   campus = "Campus Cali, U. Cooperativa de Colombia"
+   repositorio_git = "Sesion11/ejer3/Encabezado.rb"
+ 
+   # Obtener la fecha y hora actual
+   fecha_hora = Time.now.strftime("%d/%m/%Y %H:%M:%S")
+ 
+   # Imprimir el encabezado
+   puts "+----------------------------------------"
+   puts "| 👤 Nombre: #{nombre}"
+   puts "| 🎓 Campus: #{campus}"
+   puts "| 📅 Fecha y hora: #{fecha_hora}"
+   puts "| 📂 Repositorio Git: #{repositorio_git}"
+   puts "+----------------------------------------"
+   puts
+ 
+end
